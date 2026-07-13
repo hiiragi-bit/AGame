3ea4fbeb57b62ca248410a211ae2865e9ddda613
hiiragi-art <shuto.latte@gmail.com>
2026-07-13 11:03:18 +0900

    a

--- Project/GameProject/Game/Enemy.cpp
+++ Project/GameProject/Game/Enemy.cpp
@@ -12,11 +12,164 @@ Enemy::Enemy(const CVector3D& pos)
 	//スケール
 	m_scale = CVector3D(0.01f, 0.01f, 0.01f);
 	//当たり判定の半径
-	m_rad = 0.8f;
+	m_rad = 1.0f;
+	//敵のHP
+	m_hp = 100;
+	//状態変数
+	m_state = eState_Idle;
+	//攻撃フラグ
+	m_attck_flag = false;
+	//各状態の工程
+	m_state_step = 0;
+	//攻撃のクールタイム
+	m_attack_time = 0;
+	//移動用時間計測カウンタ
+	m_move_cnt = 0;
+	//接地フラグ
+	m_is_ground = true;
+}
+
+void Enemy::StateIdle()
+{
+	//キャラクターの移動量
+	const float move_speed = 0.05f;
+	m_attack_time++;
+	m_move_cnt++;
+	//ターゲットへのベクトル
+	CVector3D vec;
+	if (Base* p = Base::FindObject(ePlayer)) {
+		//ターゲットへのベクトル
+		vec = p->m_pos - m_pos;
+		//ベクトルから回転値を逆算
+		m_rot.y = atan2(vec.x, vec.z);
+	}
+	//回転値から方向ベクトルを計算
+	m_dir = (CVector3D(sin(m_rot.y), 0, cos(m_rot.y)));
+	if (vec.Length() > 1.5f) {
+		//前進
+		m_pos += m_dir * move_speed;
+		//走るアニメーション
+		m_model.ChangeAnimation(eAnim_Run);
+	}
+	else {
+		if (m_attack_time >= 60) {
+			m_state = eState_Attack;
+		}
+		else {
+			//待機アニメーション
+			m_model.ChangeAnimation(eAnim_Idle);
+		}
+	}
+	//時間経過でステップ
+	if (m_move_cnt >= 180) {
+		m_state = eState_Step;
+	}
+}
+
+void Enemy::StateStep()
+{
+	//switch (m_state_step) {
+	//case 0:
+	//{
+	//	//自身の右、後ろ、左の３方向からランダムでステップ方向を決定
+	//	int r = rand() % 3;
+	//	CVector3D dir[3] = { CVector3D(-1,0,0),CVector3D(0,0,-1),CVector3D(1,0,0) };
+	//	const float jump_pow = 0.12f;
+	//	const float step_pow = 0.08f;
+	//	m_vec = CVector3D(CMatrix::MRotationY(m_rot.y) * CVector4D(dir[r], 0)) * step_pow;
+	//	m_vec.y = jump_pow;
+	//	m_is_ground = false;
+	//}
+	//m_state_step++;
+	//m_model.ChangeAnimation(eAnim_Step, false);
+	//break;
+	//case 1:
+	//	//アニメーション終了で待機へ
+	//	if (m_model.isAnimationEnd()) {
+	//		m_move_cnt = 0;
+	//		m_state_step = 0;
+	//		m_is_ground = true;
+	//		m_state = eState_Stop;
+	//	}
+	//	break;
+	//}
+}
+
+void Enemy::StateStop()
+{
+	m_state = eState_Idle;
+}
+
+void Enemy::StateAttack()
+{
+	m_model.ChangeAnimation(eAnim_Attack, false);
+	switch (m_state_step) {
+	case 0:
+		//一定フレーム以降に攻撃フラグON
+		if (m_model.GetAnimationFrame() > 7) {
+			m_attck_flag = true;
+			m_state_step++;
+		}
+		break;
+	case 1:
+		//右腕の位置で攻撃用判定用カプセルを作成
+		m_attack_cap = CCapsule(m_model.GetFrameMatrix(30).GetPosition(),
+			m_model.GetFrameMatrix(31).GetPosition(),
+			0.2f);
+		//一定フレーム以降に攻撃フラグOFF
+		if (m_model.GetAnimationFrame() > 10) {
+			m_attck_flag = false;
+			m_state_step++;
+		}
+		break;
+	}
+	//アニメーション終了で待機へ
+	if (m_model.isAnimationEnd()) {
+		m_state_step = 0;
+		m_attack_time = 0;
+		m_state = eState_Idle;
+	}
+}
+
+void Enemy::StateDamage()
+{
+	m_model.ChangeAnimation(eAnim_Damage, false);
+	if (m_model.isAnimationEnd()) {
+		m_state = eState_Idle;
+	}
+}
+
+void Enemy::StateDown()
+{
+	m_model.ChangeAnimation(eAnim_Down, false);
+	if (m_model.isAnimationEnd()) {
+		SetKill();
+	}
 }
 
 void Enemy::Update()
 {
+	switch (m_state) {
+	case eState_Idle:
+		StateIdle();
+		break;
+	case eState_Step:
+		StateStep();
+		break;
+	case eState_Stop:
+		StateStop();
+		break;
+	case eState_Attack:
+		StateAttack();
+		break;
+	case eState_Damage:
+		StateDamage();
+		break;
+	case eState_Down:
+		StateDown();
+		break;
+	}
+
 	//重力落下
 	m_vec.y -= GRAVITY;
 	//移動
@@ -81,5 +234,11 @@ void Enemy::Collision(Base* b)
 
 void Enemy::TakeDamage(const CVector3D& vec)
 {
-	SetKill();
+	m_hp -= 50;
+	if (m_hp > 0) {
+		m_state = eState_Damage;
+	}
+	else {
+		m_state = eState_Down;
+	}
 }
--- Project/GameProject/Game/Enemy.h
+++ Project/GameProject/Game/Enemy.h
@@ -1,9 +1,60 @@
 #pragma once
 #include "../Base/Base.h"
+#include "IDamage.h"
 
-class Enemy :public Base {
+class Enemy :public Base, public IDamage {
 private:
+	//アニメーション番号
+	enum {
+		eAnim_Idle,
+		eAnim_Run,
+		eAnim_Step = 12,
+		eAnim_Attack = 20,
+		eAnim_Damage = 15,
+		eAnim_Down,
+	};
+
+	//状態
+	enum {
+		eState_Idle,
+		eState_Step,
+		eState_Stop,
+		eState_Attack,
+		eState_Damage,
+		eState_Down,
+	};
+
+	//状態変数
+	int m_state;
+
+	//通常状態
+	void StateIdle();
+
+	//ステップ状態
+	void StateStep();
+
+	//ストップ状態
+	void StateStop();
+
+	//攻撃状態
+	void StateAttack();
+
+	//ダメージ状態
+	void StateDamage();
+
+	//ダウン状態
+	void StateDown();
+
+	//敵のモデル
 	CModelA3M m_model;
+
+	int m_hp;				//敵のHP
+	bool m_attck_flag;		//攻撃フラグ
+	CCapsule m_attack_cap;	//攻撃のカプセル
+	int m_state_step;		//各状態の工程
+	int m_attack_time;		//攻撃のクールタイム
+	int m_move_cnt;			//移動用時間計測カウンタ
+	bool m_is_ground;		//接地フラグ
 
 public:
 	Enemy(const CVector3D& pos);
